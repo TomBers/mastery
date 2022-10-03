49d143b424d599b274bf8a12a3e150114371e6a0
TomBers <tjcberman@gmail.com>
2022-10-03 21:00:17 +0100

    func core testing complete

--- test/question_test.exs
+++ test/question_test.exs
@@ -14,4 +14,21 @@ defmodule QuestionTest do
 
     assert Keyword.fetch!(subsitutions, :left) == generators.left.()
   end
+
+  test "building creates a question text" do
+    question = build_question(generators: addition_generators([1], [2]))
+    assert question.asked == "1 + 2"
+  end
+
+  test "a random choice is made from the list of generators" do
+    generators = addition_generators(Enum.to_list(1..9), [0])
+
+    assert eventually_match(generators, 1)
+    assert eventually_match(generators, 9)
+  end
+
+  def eventually_match(generators, answer) do
+    Stream.repeatedly(fn -> build_question(generators: generators).subsitutions end)
+    |> Enum.find(fn sub -> Keyword.fetch!(sub, :left) == answer end)
+  end
 end
